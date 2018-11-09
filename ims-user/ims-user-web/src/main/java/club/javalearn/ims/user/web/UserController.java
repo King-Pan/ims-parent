5cc9bd36c32587c712d6eaac686670a2180d4c40
king-pan <pwpw1218@gmail.com>
2018-11-09 17:23:35 +0800

    add用户控制器

--- ims-user/ims-user-web/src/main/java/club/javalearn/ims/user/web/UserController.java
+++ ims-user/ims-user-web/src/main/java/club/javalearn/ims/user/web/UserController.java
@@ -1,10 +1,24 @@
 package club.javalearn.ims.user.web;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
 /**
  * @author king-pan
  * @date 2018/11/9
  * @Description 用户控制器
  */
+@RestController
+@RequestMapping("/user")
 public class UserController {
+
+
+    public ModelAndView page(){
+        ModelAndView view = new ModelAndView();
+        view.setViewName("system/user");
+        return view;
+    }
+
 
 }
