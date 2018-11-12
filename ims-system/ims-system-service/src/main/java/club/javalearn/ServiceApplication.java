ba7976893c37447487390450c6ca17ef35a0f949
king-pan <pwpw1218@gmail.com>
2018-11-12 18:05:00 +0800

    jpa 封装

--- ims-system/ims-system-service/src/main/java/club/javalearn/ServiceApplication.java
+++ ims-system/ims-system-service/src/main/java/club/javalearn/ServiceApplication.java
@@ -1,7 +1,11 @@
 package club.javalearn;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import javax.persistence.EntityManager;
 
 /**
  * @author king-pan
@@ -13,5 +17,13 @@ public class ServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ServiceApplication.class,args);
+    }
+
+    /**
+     *   让Spring管理JPAQueryFactory
+     */
+    @Bean
+    public JPAQueryFactory jpaQueryFactory(EntityManager entityManager){
+        return new JPAQueryFactory(entityManager);
     }
 }
--- ims-system/ims-system-service/src/main/java/club/javalearn/ims/system/service/impl/UserServiceImpl.java
+++ ims-system/ims-system-service/src/main/java/club/javalearn/ims/system/service/impl/UserServiceImpl.java
@@ -1,8 +1,11 @@
 package club.javalearn.ims.system.service.impl;
 
+import club.javalearn.ims.system.entity.QUser;
 import club.javalearn.ims.system.entity.User;
 import club.javalearn.ims.system.repository.UserRepository;
 import club.javalearn.ims.system.service.UserService;
+import com.querydsl.jpa.impl.JPAQuery;
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
@@ -20,6 +23,9 @@ public class UserServiceImpl implements UserService {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private JPAQueryFactory jpaQueryFactory;
+
     @Override
     public User findUserByName(String userName) {
         return userRepository.findByUserName(userName);
@@ -33,6 +39,9 @@ public class UserServiceImpl implements UserService {
     @Override
     public List<User> pageList(User user, Pageable pageable) {
 
+        QUser qUser = QUser.user;
+        JPAQuery<User> jpaQuery = jpaQueryFactory.select(qUser);
+        //jpaQuery.select()
 
         return null;
     }
